06c59bdb23f1b1291006215bd1c4349a92a75109
casperlamboo <casperlamboo@gmail.com>
2016-03-20 08:28:17 +0100

    update example

--- example/index.js
+++ example/index.js
@@ -1,40 +1,10 @@
-import Canvas from '/src/HTMLCanvasElement.js';
-import Image from '/src/Image.js';
+import { transfer } from '/src/index.js';
+import Worker from './WorkerShim';
 
-const canvas = new Canvas(500, 500);
-const context = canvas.getContext('2d');
+const worker = new Worker('./worker.js');
 
-const realCanvas = document.createElement('canvas');
-realCanvas.width = 500;
-realCanvas.height = 500;
-document.body.appendChild(realCanvas);
-const realContext = realCanvas.getContext('2d');
+worker.addEventListener('message', ({ data }) => {
+  const canvas = transfer.decode(data);
 
-const pattern = new Image();
-pattern.onload = function() {
-
-  const gradient = new Image();
-  gradient.onload = function() {
-    context.drawImage(pattern, 0, 0);
-    context.globalCompositeOperation = 'xor';
-    context.drawImage(gradient, 0, 0);
-
-    context.rect(100, 100, 100, 100);
-
-    context.fill();
-
-    console.log(canvas);
-
-    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-
-    const realImageData = realContext.createImageData(canvas.width, canvas.height);
-
-    for (let i = 0; i < imageData.data.length; i ++) {
-      realImageData.data[i] = imageData.data[i];
-    }
-
-    realContext.putImageData(realImageData, 0, 0);
-  };
-  gradient.src = './img/gradient.png';
-};
-pattern.src = './img/pattern.png';
+  document.body.appendChild(canvas);
+});
---
+++ example/worker.js
@@ -0,0 +1,10 @@
+import { Canvas, Image, transfer } from '/src/index.js';
+
+const canvas = new Canvas(300, 300);
+const context = canvas.getContext('2d');
+
+context.fillRect(100, 100, 100, 100);
+
+const { data, buffer } = transfer.encode(canvas);
+
+self.postMessage(data, [buffer]);
